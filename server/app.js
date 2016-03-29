780e18e07e5e7bf5da48f734148ae901232503dc
Julien <juliengenoud@gmail.com>
2016-03-29 22:39:31 +0800

    server update

--- server/app.js
+++ server/app.js
@@ -4,9 +4,41 @@ var http = require('http');
 //Lets define a port we want to listen to
 const PORT=8080;
 
+
+var commands = [];
+
+function handlejson(json) {
+
+}
+
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+
+
+  if(request.method === "POST") {
+    if (request.url === "/") {
+
+      response.writeHead(200, {'Content-Type': 'text/html'});
+
+      // result =  handlejson(json);
+
+//    draw response
+      response.end("JSON REÇU" + request);
+
+    //  handlejson(json);
+      // verify last remaining time
+      // if the angle is not ok try to correct —> final step
+      // determine where is the car on the map
+      // draw the map
+      // define target destination
+      // set direction
+      // store commands
+
+    }
+  }
+  else {
+    response.end('Incorrect Request' + request.url);
+  }
 }
 
 //Create a server
@@ -16,4 +48,5 @@ var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
+    //
 })
